1baa2e7a9e90ecbfcf4082266844fbcfdb6a4f42
laucherish <laucherish@gmail.com>
2017-10-04 17:37:30 +0800

    Init commit

---
+++ app/src/main/java/com/laucherish/downloadtest/DownloadInterface.java
@@ -0,0 +1,18 @@
+package com.laucherish.downloadtest;
+
+/**
+ * Created by laucherish on 2017/10/4.
+ */
+
+public interface DownloadInterface {
+
+    void onProgress(int progress);
+
+    void onSuccess();
+
+    void onFailed();
+
+    void onPaused();
+
+    void onCanceled();
+}
---
+++ app/src/main/java/com/laucherish/downloadtest/DownloadTask.java
@@ -0,0 +1,15 @@
+package com.laucherish.downloadtest;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by laucherish on 2017/10/4.
+ */
+
+public class DownloadTask extends AsyncTask<String, Integer, Integer> {
+
+    @Override
+    protected Integer doInBackground(String... strings) {
+        return null;
+    }
+}
