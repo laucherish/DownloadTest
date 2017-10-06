7badbf5612093abc00ecdfe067c3e029a46ca497
laucherish <laucherish@gmail.com>
2017-10-06 11:45:56 +0800

    Add Toolbar

--- app/src/main/java/com/laucherish/downloadtest/MainActivity.java
+++ app/src/main/java/com/laucherish/downloadtest/MainActivity.java
@@ -11,6 +11,7 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -37,6 +38,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         mBtnStart = (Button) findViewById(R.id.btn_start);
         mBtnPause = (Button) findViewById(R.id.btn_pause);
         mBtnCancel = (Button) findViewById(R.id.btn_cancel);
