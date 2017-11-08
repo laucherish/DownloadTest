719b268727b4ca4aeadc7358f36fb8cbf025ca39
laucherish <laucherish@gmail.com>
2017-11-08 08:49:05 +0800

    Add Toolbar

--- app/src/main/java/com/laucherish/downloadtest/MainActivity.java
+++ app/src/main/java/com/laucherish/downloadtest/MainActivity.java
@@ -36,6 +36,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
