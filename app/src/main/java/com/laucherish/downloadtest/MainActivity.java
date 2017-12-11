e54144c660d53a73316244cf2c744541b1d0ca4d
laucherish <laucherish@gmail.com>
2017-12-11 09:51:35 +0800

    Add Toolbar

--- app/src/main/java/com/laucherish/downloadtest/MainActivity.java
+++ app/src/main/java/com/laucherish/downloadtest/MainActivity.java
@@ -81,6 +81,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btn_stop:
                 stopService(new Intent(MainActivity.this, DownloadService.class));
                 break;
+
         }
     }
 
