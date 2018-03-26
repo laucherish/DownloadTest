74353bb9d385e5be7428c2ada8c33a3b3f75534c
laucherish <laucherish@gmail.com>
2018-03-26 10:25:57 +0800

    fix bugs

--- app/src/main/java/com/laucherish/downloadtest/MainActivity.java
+++ app/src/main/java/com/laucherish/downloadtest/MainActivity.java
@@ -77,7 +77,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
 
-
                 break;
             case R.id.btn_cancel:
                 mBinder.cancelDownload();
