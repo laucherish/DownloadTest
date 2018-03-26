9e5b88090529207404a794c1acd9a4e5b72116c3
laucherish <laucherish@gmail.com>
2018-03-26 10:26:21 +0800

    fix bugs

--- app/src/main/java/com/laucherish/downloadtest/MainActivity.java
+++ app/src/main/java/com/laucherish/downloadtest/MainActivity.java
@@ -69,6 +69,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         switch (view.getId()) {
             case R.id.btn_start:
+
                 String url = "http://ftp.jaist.ac.jp/pub/eclipse/oomph/epp/oxygen/R/eclipse-inst-mac64.tar.gz";
                 mBinder.startDownload(url);
                 break;
