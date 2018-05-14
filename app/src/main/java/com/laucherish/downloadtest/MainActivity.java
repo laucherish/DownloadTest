82fbfc2097ae373711e8555554e9b3f5546d3c31
laucherish <laucherish@gmail.com>
2018-05-14 15:22:12 +0800

    fix bugs

--- app/src/main/java/com/laucherish/downloadtest/MainActivity.java
+++ app/src/main/java/com/laucherish/downloadtest/MainActivity.java
@@ -52,6 +52,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mBtnStop.setOnClickListener(this);
 
 
+
         Intent intent = new Intent(this, DownloadService.class);
         startService(intent);
         bindService(intent, mConnection, BIND_AUTO_CREATE);
